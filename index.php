f341f857573e1cdd6771aa07cc72d07a7f8209f5
andrei <work4program@gmail.com>
2021-11-30 20:36:28 +0700

    chamge index

--- index.php
+++ index.php
@@ -8,11 +8,11 @@ require_once 'config/params.php';
 
 require_once 'module/autoload.php';
 
+$db = new blog\module\DbConnect(DB);
+
 $param = explode('/', $_SERVER['REQUEST_URI']);
 
 $action = strlen($param[1]) ? htmlspecialchars($param[1]) : 'index';
-
-$db = new blog\module\DbConnect(DB);
 
 $fullname = 'controllers/' . $action . '.php';
 
